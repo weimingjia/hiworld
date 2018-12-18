7a9b12ceb13b4e9b20dbd5851f1556ed582ce3a9
Mango <995779872@qq.com>
2018-12-18 14:45:13 +0800

    khkh

---
+++ src/main/java/com/itheima/AA.java
@@ -0,0 +1,26 @@
+package com.itheima;
+
+/*
+ *  @项目名：  HiSpringBoot
+ *  @包名：    com.itheima
+ *  @文件名:   AA
+ *  @创建者:   admin
+ *  @创建时间:  2018/9/18 19:50
+ *  @描述：    TODO
+ */
+public class AA {
+
+//    private int a:
+
+    public static void main(String [] args){
+
+
+       /* FileInputStream s = new FileInputStream(name:"");
+        FileInputStream s1 = new FileInputStream(name:"");
+        FileInputStream s1 = new FileInputStream(name:"");
+//       FileInputStream s2= new FileInputStream(name:"");
+
+*/
+    }
+
+}
---
+++ src/main/java/com/itheima/MainApp.java
@@ -0,0 +1,21 @@
+package com.itheima;
+
+/*
+ *  @项目名：  HiSpringBoot
+ *  @包名：    com.itheima
+ *  @文件名:   MainApp
+ *  @创建者:   admin
+ *  @创建时间:  2018/9/18 19:37
+ *  @描述：    TODO
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+
+    public static void main(String [] args){
+        SpringApplication.run(MainApp.class,args);
+    }
+}
